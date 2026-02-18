0ccc05190edee46d5ede1b33d3bc01505c2914e7
Strivex72 <ashrafulnayeem72@gmail.com>
2026-02-18 13:31:27 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+    $(document).ready(function(){
+        $('.sale-products-slider').slick({
+            vertical: true,
+            verticalSwiping: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            arrows: false,
+            dots: false
+        });
+    });
