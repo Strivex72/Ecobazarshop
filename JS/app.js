725793b47c4b5f97db96506a697f00934864869f
Strivex72 <ashrafulnayeem72@gmail.com>
2026-02-22 13:52:11 +0600

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -9,3 +9,34 @@
             dots: false
         });
     });
+
+ $('.number-slider').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  infinite: false,
+  arrows: false,
+  dots: false,
+  variableWidth: true, 
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3
+      }
+    }
+  ]
+});
+
+$('.prev-btn').click(function() {
+  $('.number-slider').slick('slickPrev');
+});
+
+$('.next-btn').click(function() {
+  $('.number-slider').slick('slickNext');
+});
